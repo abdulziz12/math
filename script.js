ebc101ce501b94f1812660c44ef52f82ddcf5552
abdulziz12 <abdurashidov200930@gmail.com>
2024-06-18 18:31:29 +0500

    u6yt67tiy
    njhnbgf

---
+++ script.js
@@ -0,0 +1,10 @@
+var name1 = prompt("string typedagi malumot kiriting")
+var bir = prompt("number typedagi malumot kiriting")
+var ikki = prompt("boolean typedagi malumot kiriting")
+
+alert(`Siz kiritgan ${name1} sozi string typega tegishli
+Siz kiritgan ${bir} soni numberga typega tegishli
+Siz kiritgan ${name1} sozi boolean typega tegishli`)
+
+var userResponse = confirm
+
